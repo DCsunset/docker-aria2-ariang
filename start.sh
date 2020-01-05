b7c1daac78c7317bff29a988a53310c5a51e7933
DCsunset <dcsunset@protonmail.com>
2020-01-05 02:09:43 -0800

    fix: not to listen on all ports by default

--- start.sh
+++ start.sh
@@ -5,14 +5,12 @@ nginx
 if [ $RPC_SECRET ]; then
 	aria2c --conf-path=/app/conf/aria2.conf \
 		--enable-rpc \
-		--rpc-listen-all \
 		--rpc-secret="$RPC_SECRET" \
 		--rpc-listen-port=6800 \
 		--dir=/data
 else
 	aria2c --conf-path=/app/conf/aria2.conf \
 		--enable-rpc \
-		--rpc-listen-all \
 		--rpc-listen-port=6800 \
 		--dir=/data
 fi

c4688c25540ec3229b62b2deec47980bdf1dfab1
DCsunset <dcsunset@protonmail.com>
2020-01-04 21:01:13 -0800

    fix: fix when RPC_SECRET is not defined

--- start.sh
+++ start.sh
@@ -2,9 +2,17 @@
 
 nginx
 
-aria2c --conf-path=/app/conf/aria2.conf \
-	--enable-rpc \
-	--rpc-listen-all \
-	--rpc-secret="$RPC_SECRET" \
-	--rpc-listen-port=6800 \
-	--dir=/data
+if [ $RPC_SECRET ]; then
+	aria2c --conf-path=/app/conf/aria2.conf \
+		--enable-rpc \
+		--rpc-listen-all \
+		--rpc-secret="$RPC_SECRET" \
+		--rpc-listen-port=6800 \
+		--dir=/data
+else
+	aria2c --conf-path=/app/conf/aria2.conf \
+		--enable-rpc \
+		--rpc-listen-all \
+		--rpc-listen-port=6800 \
+		--dir=/data
+fi
